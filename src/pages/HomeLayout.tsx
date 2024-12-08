d47a3e59e9f824183e2ad3647bd7f95b3c90c124
Dinesh <dineshlegha23@gmail.com>
2024-12-08 18:06:42 +0530

    added global loading

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,17 +1,18 @@
-import { Header } from "@/components";
-import Navbar from "@/components/Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import { Header, Navbar, Loading } from "@/components";
 
-function HomeLayout() {
+const HomeLayout = () => {
+  const navigation = useNavigation();
+  const isPageLoading = navigation.state === "loading";
   return (
     <>
       <Header />
       <Navbar />
+
       <div className="align-element py-20">
-        <Outlet />
+        {isPageLoading ? <Loading /> : <Outlet />}
       </div>
     </>
   );
-}
-
+};
 export default HomeLayout;
